7e91cf618f01c81557bbdda3052a4b409705156c
Renato Scop <r.scop@hotmail.com>
2021-08-26 10:54:39 -0300

    fixing deploy-commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -5,12 +5,14 @@ const { Routes } = require('discord-api-types/v9');
 const { token, clientId, guildId } = require('./config.json')
 
 const commands = [
-	new SlashCommandBuilder().setName('cryptos').setDescription('Replies with all TISP Cryptos Currencies!'),
-	new SlashCommandBuilder().setName('crypto').setDescription('Replies with all TISP Cryptos Currencies!')
-    .addStringOption(option => option.setName('actualcrypto').setDescription('Enter Crypto to get Currrency')),
-	new SlashCommandBuilder().setName('currency').setDescription('Replies crypto currency conversion')
-    .addStringOption(option => option.setName('checkcrypto').setDescription('Enter Crypto to Convert'))
-    .addStringOption(option => option.setName('quantity').setDescription('Enter quantity'))
+	new SlashCommandBuilder().setName('getcryptos').setDescription('Replies with all TISP Cryptos Currencies!'),
+	new SlashCommandBuilder().setName('crypto').setDescription('Replies with specific TISP Cryptos Currencies!')
+    .addStringOption(option => option.setName('crytptoname').setDescription('Enter Crypto to get Currrency')),
+	new SlashCommandBuilder().setName('exchange').setDescription('Replies crypto currency conversion')
+    .addStringOption(option => option.setName('cryptotoexchange').setDescription('Enter the Crypto'))
+    .addStringOption(option => option.setName('quantity').setDescription('Enter the quantity')),
+	new SlashCommandBuilder().setName('help').setDescription('Replies with all commands'),
+	new SlashCommandBuilder().setName('cryptostag').setDescription('Replies all TISP cryptos TAGs')
 ].map(command => command.toJSON())
 
 const rest = new REST({ version: '9' }).setToken(token);
